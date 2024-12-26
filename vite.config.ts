cad5ae87e6de65e82cfcd05ebbacf86ab5fe8a07
0xk3rn3lp4nic <dev@itsrabayadg.com>
2024-12-26 15:40:27 +0800

    Added the SSL on config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    https: {
+      key: resolve(__dirname, "ssl/key.pem"), // Path to your private key
+      cert: resolve(__dirname, "ssl/cert.pem"), // Path to your certificate
+    },
+  },
+});
