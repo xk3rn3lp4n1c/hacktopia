dcc57a6cf133b49d45c86db3489370f9cf2ef26f
0xk3rn3lp4nic <dev@itsrabayadg.com>
2024-12-26 23:22:10 +0800

    Added Check Team API

--- api/team/team.ts
+++ api/team/team.ts
@@ -2,20 +2,21 @@
 
 import { APP_API_ENDPOINT_URL } from "../api_endpoint";
 import axios from "axios";
-import { getToken } from "../token";
 
 export const CreateTeam = async ({
   teamName = "",
   teamCode = "",
   teamCaptain = "",
+  token = "", // Add token as a parameter
 }: {
   teamName: string;
   teamCode: string;
   teamCaptain: string;
+  token: string; // Add token to the type definition
 }) => {
   try {
     const response = await axios.post(
-      `${APP_API_ENDPOINT_URL}/api/v1/auth/login`,
+      `${APP_API_ENDPOINT_URL}/api/v1/team/create`,
       {
         teamName,
         teamCode,
@@ -23,7 +24,29 @@ export const CreateTeam = async ({
       },
       {
         headers: {
-          Authorization: `Bearer ${getToken()}`,
+          Authorization: `Bearer ${token}`, // Use the passed token
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const ChkTeam = async ({
+  teamName = "",
+  token = "",
+}: {
+  teamName: string;
+  token: string;
+}) => {
+  try {
+    const response = await axios.get(
+      `${APP_API_ENDPOINT_URL}/api/v1/team/chk-team?teamName=${teamName}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
         },
       }
     );
