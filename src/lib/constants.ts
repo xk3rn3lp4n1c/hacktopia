702fd4349b66799fb707fecdb7375464ff2e18e1
0xk3rn3lp4nic <dev@itsrabayadg.com>
2024-12-26 15:24:32 +0800

    Added global constant variables

---
+++ src/lib/constants.ts
@@ -0,0 +1,6 @@
+import logo from "/app-logo.svg";
+
+export const APP_NAME = "Hacktopia";
+export const APP_DESC = "The Ultimate Capture The Flag Platform";
+export const APP_LOGO = logo;
+export const APP_VERSION = '1.0.0'
