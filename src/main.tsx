993b78814fab2a98ef75b0e4f6ccbd564d9d93fa
0xk3rn3lp4nic <dev@itsrabayadg.com>
2024-12-26 15:39:04 +0800

    Installed react router and react cookie

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { CookiesProvider } from "react-cookie";
+import ReduxProvider from "./providers/redux-provider.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./components/ui/theme-provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ReduxProvider>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <CookiesProvider defaultSetOptions={{ path: "/" }}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </CookiesProvider>
+      </ThemeProvider>
+    </ReduxProvider>
+  </StrictMode>
+);
