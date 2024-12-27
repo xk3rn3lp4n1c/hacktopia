9c2e52a57cdb4d369fa5cb790066bc85c9d067bd
0xk3rn3lp4nic <dev@itsrabayadg.com>
2024-12-27 21:00:26 +0800

    Added some socketServer constant

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { io } from "socket.io-client";
+import { APP_API_ENDPOINT_URL } from "../../api/api_endpoint";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const socketServer = io(APP_API_ENDPOINT_URL);
