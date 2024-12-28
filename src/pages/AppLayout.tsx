f3dc79f74561ee2243bfc6abd192e360d82e292f
0xk3rn3lp4nic <dev@itsrabayadg.com>
2024-12-28 13:29:39 +0800

    Refactored

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,16 +1,30 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Navbar from "./components/Navbar";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
+  const navRef = useRef<HTMLElement>(null); // Ref for the Navbar
+  const [navHeight, setNavHeight] = useState<number>(0); // State to store Navbar height
+
+  // Effect to calculate and set the Navbar height
+  useEffect(() => {
+    if (navRef.current) {
+      setNavHeight(navRef.current.offsetHeight);
+    }
+  }, []);
   return (
     <div className="w-[100vw] h-[100vh] overflow-hidden">
-      <ScrollArea className="grid grid-rows-[auto_1fr] w-[100vw] h-[100vh]">
-        <Navbar />
-        <main>{children}</main>
-      </ScrollArea>
+      <div className="grid grid-rows-[auto_1fr]">
+        <Navbar ref={navRef} />
+        <main
+          className="w-full"
+          style={{ height: `calc(100vh - ${navHeight}px)` }}
+        >
+          {children}
+        </main>
+      </div>
     </div>
   );
 };
