e7be3ba150a2b190d60385934f45fe698cf4237b
0xk3rn3lp4nic <dev@itsrabayadg.com>
2025-01-03 18:27:22 +0800

    Add dropdown menu for join requests in MyTeam component

--- src/pages/team/MyTeam.tsx
+++ src/pages/team/MyTeam.tsx
@@ -3,10 +3,19 @@ import { GetMyTeamDetails } from "../../../api/team/team";
 import React, { useEffect } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Separator } from "@radix-ui/react-separator";
+import { CrownIcon, Notification02Icon } from "hugeicons-react";
+import Jdenticon from "react-jdenticon";
+
 import {
-  CrownIcon,
-} from "hugeicons-react";
-import { Jdenticon } from "react-jdenticon";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal, MoreVertical } from "lucide-react";
 
 const MyTeam = () => {
   const [teamData, setTeamData] = React.useState<any>();
@@ -14,14 +23,12 @@ const MyTeam = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      await GetMyTeamDetails({ userId: userId, token: token }).then(
-        async (res) => {
-          if (res) {
-            setTeamData(res);
-            console.log(res);
-          }
+      await GetMyTeamDetails({ userId: userId, token: token }).then((res) => {
+        if (res) {
+          setTeamData(res);
+          console.log(res);
         }
-      );
+      });
     };
 
     fetchData();
@@ -32,11 +39,6 @@ const MyTeam = () => {
       <div>
         <div className="space-y-6">
           <div className="w-full flex justify-between items-start">
-            {teamData && teamData.team.teamName ? (
-              <Jdenticon size="80" value={teamData.team.teamName} />
-            ) : (
-              <Skeleton className="h-20 w-20" />
-            )}
             <div>
               {teamData && teamData.team.teamRanking ? (
                 <div className="grid place-items-center gap-2">
@@ -53,12 +55,71 @@ const MyTeam = () => {
                 <Skeleton className="h-20 w-20" />
               )}
             </div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="ghost"
+                  className="flex flex-row justify-start items-center gap-2"
+                >
+                  <Notification02Icon className="h-4 w-4" />
+                  Join Requests
+                  {teamData && teamData.teamRequests.length > 0 && (
+                    <span className="text-background text-xs grid place-items-center h-5 w-5 bg-red-500 rounded-full">
+                      {teamData.teamRequests.length}
+                    </span>
+                  )}
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent side="bottom" align="end">
+                <DropdownMenuLabel>Join Requests</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <div>
+                  {teamData &&
+                    teamData.teamRequests.map((request: any) => {
+                      return (
+                        <div
+                          key={request.id}
+                          className="flex flex-row justify-start items-center gap-4 hover:bg-accent p-2 rounded-lg"
+                        >
+                          <Jdenticon
+                            size={32}
+                            value={request.user.profiles.userName}
+                          />
+                          <div className="flex flex-col">
+                            <span>{request.user.profiles.userName}</span>
+                            <span className="text-xs capitalize">
+                              {request.createdAt}
+                            </span>
+                          </div>
+                          <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                              <Button variant="ghost" size={"sm"}>
+                                <MoreVertical className="h-4 w-4" />
+                              </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                              <DropdownMenuSeparator />
+                              <DropdownMenuItem>Profile</DropdownMenuItem>
+                              <DropdownMenuItem>Billing</DropdownMenuItem>
+                              <DropdownMenuItem>Team</DropdownMenuItem>
+                              <DropdownMenuItem>Subscription</DropdownMenuItem>
+                            </DropdownMenuContent>
+                          </DropdownMenu>
+                        </div>
+                      );
+                    })}
+                </div>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
           <div className="space-y-4">
             <div className="flex flex-row justify-between items-center">
               <div>
                 <h1 className="text-2xl font-bold flex flex-row justify-start items-center gap-4">
-                  {teamData && teamData.team.teamName || <Skeleton className="h-8 w-32" />}{" "}
+                  {(teamData && teamData.team.teamName) || (
+                    <Skeleton className="h-8 w-32" />
+                  )}{" "}
                 </h1>
                 <span className="text-muted-foreground text-sm">
                   {teamData && teamData.team.teamMotto ? (
